23eb9cfde832fe000a16672c8daa3cfb9608c846
Andrew Pohl <andrewpohl@Andrews-MBP-2.att.net>
2015-02-02 22:51:25 -0800

    Was having a hard time with the pop method.. confused because it keeps pulling the index at the end of the array

--- javascripts/naive_queue.js
+++ javascripts/naive_queue.js
@@ -1,14 +1,17 @@
 // Write the constructor
 function Queue() {
   // a list attribute
+  this.list = [];
 } 
 
 Queue.prototype.push = function(val){
   // push a val into the back of the list
-  
+  this.list.push(val);
   // return this
+  return this;
 };
 
 Queue.prototype.pop = function(){
   // remove a val from the front of the list
+  return this.list.shift();
 };
