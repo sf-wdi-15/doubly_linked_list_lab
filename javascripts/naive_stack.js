75eb660ca864152e57c8204f1de4c60f7086a243
Angelo Cisneros <a.e.cisneros@gmail.com>
2015-02-02 19:05:50 -0800

    Homework submission

--- javascripts/naive_stack.js
+++ javascripts/naive_stack.js
@@ -1,15 +1,17 @@
 // Write the constructor
 function Queue() {
   // a list attribute
+  this.list = [];
 } 
 
+// extend Queue with prototype methods
 Queue.prototype.push = function(val){
   // push a val into the back of the list
-
+  this.list.push(val);
   // return this
+  return this;
 };
-
 Queue.prototype.pop = function(){
   // remove a val from the front of the list
-
+  return this.list.pop();
 };
