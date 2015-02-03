ebc53df42e1b9ca17c52cd29657f52c0b72812ca
karenchu <karen.alison.chu@gmail.com>
2015-02-02 22:22:31 -0800

    completed

--- ruby_related/naive_queue.rb
+++ ruby_related/naive_queue.rb
@@ -3,18 +3,21 @@ class Queue
 
   def initialize
     # start new list, i.e. the array
+    @list = []
   end
 
   def push(val)
     # push val to back
     #   of list
-
+    @list.push(val)
     # return self
+    self  
   end
 
   def pop
     # return a val at the
     #   front of the list
+    @list.shift
   end
 
 end
